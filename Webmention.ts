076834fed4ded9bbbb345718e22204f232b5dc0c
Seth Van de Veire <seth.vandeveire@student.odisee.be>
2024-12-13 17:18:49 +0100

    get webmention

--- Webmention.ts
+++ Webmention.ts
@@ -1,13 +1,39 @@
 
-const sourceUrl = "https://workshop-webmentions.vercel.app/blogpost1/"; 
-const targetUrl = "https://webmention.io/webmention-client.vercel.app/webmention";
+const sourceUrl = "https://webmention.io/webmention-client.vercel.app/webmention"; 
+const targetUrl = "https://webmention-client.vercel.app/blogpost1/";
 
+async function getWebmentionEndpoint(target: string): Promise<string> {
+  try {
+    const response = await fetch(target, {
+      method: "GET",
+    });
 
-const webmentionEndpoint = `${targetUrl}?source=${encodeURIComponent(sourceUrl)}&target=${encodeURIComponent(targetUrl)}`;
+    if (response.ok) {
+      const endpoint = await response.text(); 
+      console.log("Webmention endpoint retrieved:", endpoint);
+      return endpoint;
+    } else {
+      console.error("Failed to retrieve webmention endpoint.", await response.text());
+    }
+  } catch (error) {
+    console.error("Error while retrieving webmention endpoint:", error);
+  }
+
+  return ""; 
+}
 
 async function sendWebmention() {
   try {
-    const response = await fetch(webmentionEndpoint, {
+    const webmentionEndpoint = await getWebmentionEndpoint(targetUrl);
+
+    if (!webmentionEndpoint) {
+      console.error("No endpoint available. Aborting webmention send.");
+      return;
+    }
+
+    const endpointWithParams = `${webmentionEndpoint}?source=${encodeURIComponent(sourceUrl)}&target=${encodeURIComponent(targetUrl)}`;
+
+    const response = await fetch(endpointWithParams, {
       method: "POST",
       headers: {
         "Content-Type": "application/x-www-form-urlencoded",
